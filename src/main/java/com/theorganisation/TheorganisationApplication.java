f5274aff05d5bc6f870837f1c0f2fd5e123d3910
henr0t <h.trieu@live.nl>
2020-07-26 22:06:42 +0200

    project start

---
+++ src/main/java/com/theorganisation/TheorganisationApplication.java
@@ -0,0 +1,13 @@
+package com.theorganisation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheorganisationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheorganisationApplication.class, args);
+    }
+
+}
